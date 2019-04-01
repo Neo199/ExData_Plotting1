3ec148daf7d58e95f496ef4173cbbd41487eda48
Neo199 <46858881+Neo199@users.noreply.github.com>
2019-04-01 18:01:51 +0530

    Add files via upload

--- plot3.r
+++ plot3.r
@@ -1,20 +1,19 @@
-library("data.table")
+library("data.table") 
 
-#Reads in data from file then subsets data for specified dates
-powerDT <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+#Reads in data 
+col <- c(Voltage="numeric", Global_active_power="numeric", Global_intensity="numeric" ,Sub_metering_1="numeric", Sub_metering_2="numeric",  Sub_metering_3="numeric", Global_active_power="numeric", Global_reactive_power="numeric")
+DT <-read.table("household_power_consumption.txt", header=TRUE, sep=";",dec=".", stringsAsFactors=FALSE, na.strings = "?",colClasses=col)
+# Change Date Column to Date Type
+neededDT <- DT[DT$Date %in% c("1/2/2007","2/2/2007"),]
+datetime <- strptime(paste(neededDT$Date, neededDT$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+Global_Active_Power <- as.numeric(neededDT$Global_active_power)
 
-#For no Scientific Notation
-powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
-powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
-
-# Filter Dates for 2007-02-01 and 2007-02-02
-powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
-
-png("plot3.png", width=480, height=480)
+png("plot3.png")
 
 # Plot 3
-plot(powerDT[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
-lines(powerDT[, dateTime], powerDT[, Sub_metering_2],col="red")
-lines(powerDT[, dateTime], powerDT[, Sub_metering_3],col="blue")
-legend("topright", col=c("black","red","blue"), c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),lty=c(1,1), lwd=c(1,1))
+plot(datetime, neededDT$Sub_metering_1, type="l", ylab="Energy sub metering")
+lines(powerDT$dateTime, powerDT$Sub_metering_2,col="red")
+lines(powerDT$dateTime, powerDT$Sub_metering_3,col="blue")
+legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
+
 dev.off()

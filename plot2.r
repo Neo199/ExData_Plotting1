45739a5c3abb259776560a7fe6d1b7000fc0f152
Neo199 <46858881+Neo199@users.noreply.github.com>
2019-04-01 17:56:30 +0530

    Add files via upload

--- plot2.r
+++ plot2.r
@@ -1,15 +1,14 @@
 library("data.table") 
 
 #Reads in data 
-col <- c(Voltage="numeric", Global_active_power="numeric", Global_intensity="numeric" ,Sub_metering_1="numeric", Sub_metering_2="numeric",  Sub_metering_3="numeric", Global_active_power="numeric", Global_reactive_power="numeric")
-DT <-read.table("household_power_consumption.txt", header=TRUE, sep=";",dec=".", stringsAsFactors=FALSE, na.strings = "?",colClasses=col)
+DT <-read.table("household_power_consumption.txt", header=TRUE, sep=";",dec=".", stringsAsFactors=FALSE, na.strings = "?")
 # Change Date Column to Date Type
 neededDT <- DT[DT$Date %in% c("1/2/2007","2/2/2007"),]
-as.Date(neededDT$Date)
-na.omit(neededDT)
+datetime <- strptime(paste(neededDT$Date, neededDT$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+Global_Active_Power <- as.numeric(neededDT$Global_active_power)
+png("plot2.png")
 
 ## Plot 2
-png("plot2.png")
-plot(x = neededDT$dateTime, y = neededDT$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(datetime,Global_Active_Power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 
 dev.off()
